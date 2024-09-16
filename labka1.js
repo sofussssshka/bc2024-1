0404270e4481298978729563fa006d1149613d58
Sofusssshka bc2024-1 <mulyavkosofiya@mail.com>
2024-09-16 09:43:22 +0300

    CHANGED FILE AGAIN

--- labka1.js
+++ labka1.js
@@ -1,5 +1,1 @@
-<<<<<<< HEAD
-console.log ('Hello, sofussssshka');
-=======
-console.log ('Hello, sofussssshka');
->>>>>>> 2e30e35da33feb206b55961677a2c853655e6984
+console.log("Hello, sofussssshka")
